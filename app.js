7e9e6e0e776df14e8e662c376955f94bf635bcf2
Gabriel <gabriel.akacoll@gmail.com>
2019-05-06 14:08:04 -0300

    first

---
+++ app.js
@@ -0,0 +1,30 @@
+// grab the packages we need
+let express = require('express');
+const fs = require('fs');
+let jsonGen = require('./jsonGenerator.js');
+
+let app = express();
+let port = process.env.PORT || 8080;
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// POST http://localhost:8080/api/csv
+app.post('/api/csv', function(req, res) {
+    let csv = req.body.csvFile;
+    // receives the csv path and returns a json
+    jsonGen.json(csv);
+    let json;
+    fs.readFile('./output.json', function (err, data) {
+        if (err) throw err;
+        json = data;
+    });
+    setTimeout(function(){
+        console.log("Log app.js: " + json);
+        res.send(JSON.parse(json));
+    },100);
+});
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
